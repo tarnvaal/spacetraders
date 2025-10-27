b3e60ae65c89342362b31dc5dcb775e3f9f32ce6
Eric Carpenter <ericcarpenter.mobile@gmail.com>
2025-10-26 20:02:47 -0700

    intial mining work

---
+++ logic/mine.py
@@ -0,0 +1,33 @@
+from api.client import ApiClient
+from data.warehouse import Warehouse
+from data.enums import WaypointTraitType, ShipRole
+
+class mine():
+    def __init__(self, client: ApiClient, warehouse: Warehouse):
+        self.client = client
+        self.warehouse = warehouse
+
+    def mine(self):
+        print("Mining excavators starting...")
+        self.print_fleet()
+
+    def print_fleet(self):
+        for ship in self.warehouse.ships_by_symbol.values():
+            if ship.registration and ship.registration.role == ShipRole.EXCAVATOR:
+                print(
+                    f"Mining Ship:{ship.symbol}\n"
+                    f"  Role:{ship.registration.role}\n"
+                    f"  Fuel:{ship.fuel.current} / {ship.fuel.capacity}\n"
+                    f"  Location:{ship.nav.waypointSymbol}\n"
+                    f"  Nav:{ship.nav.status} @ {ship.nav.systemSymbol}/{ship.nav.waypointSymbol}\n"
+                    f"  Flight Mode: {ship.nav.flightMode}\n"
+                    f"  Route: {ship.nav.route.departure.symbol}\n"
+                    f"         {ship.nav.route.destination.symbol}\n"
+                    f"  Times: depart {ship.nav.route.departureTime}\n"
+                    f"         arrive {ship.nav.route.arrival}\n"
+                    f"  Engine: {ship.engine.symbol} (speed {ship.engine.speed})\n"
+                    f"  Cargo: {ship.cargo.units} / {ship.cargo.capacity}\n"
+                    f"  Cooldown: {ship.cooldown.remainingSeconds} / {ship.cooldown.totalSeconds}"
+                )
+                
+    
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from api.client import ApiClient
 from data.warehouse import Warehouse
 from data.enums import WaypointTraitType
 from logic.scanner import scanner
+from logic.mine import mine
 
 # load environment variables
 load_dotenv()
@@ -20,8 +21,11 @@ config = configparser.ConfigParser()
 config_path = os.path.join(os.path.dirname(__file__), "config.ini")
 if os.path.exists(config_path):
     config.read(config_path)
+
 scan_shipyards = config.getboolean("scanner", "scan_shipyards", fallback=False) if config.has_section("scanner") else False
 print_shipyards = config.getboolean("scanner", "print_shipyards", fallback=False) if config.has_section("scanner") else False
+print_fleet = config.getboolean("scanner", "print_fleet", fallback=False) if config.has_section("scanner") else False
+
 scanner.print_hq_system()
 scanner.scan_systems()
 if scan_shipyards:
@@ -29,5 +33,9 @@ if scan_shipyards:
 if print_shipyards:
     scanner.print_waypoints_by_type(WaypointTraitType.SHIPYARD)
 scanner.scan_fleet()
-scanner.print_fleet()
+if print_fleet:
+    scanner.print_fleet()
 dataWarehouse.print_warehouse_size()
+
+mine = mine(client, dataWarehouse)
+mine.mine()
